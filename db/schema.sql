3d3db14b52ee305286c6d14aa01aab1ff7e1a9ab
Daryl Hornick-Becker <darylhornickbecker@gmail.com>
2025-05-20 22:13:01 -0400

    Initial Steps

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,10 @@
 DROP TABLE IF EXISTS employees;
 
+CREATE TABLE employees(
+    id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL,
+    birthday DATE default now() NOT NULL,
+    salary INTEGER NOT NULL
+)
+
 
